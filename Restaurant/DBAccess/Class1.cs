120fc5462d7822930700992a0076e4c0e2cf0c44
Taha Biklaryan <taha.biglarian@gmail.com>
2024-06-14 12:36:31 +0330

    Data Base Access class

---
+++ Restaurant/DBAccess/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.SqlClient; 
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlTypes;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+
+namespace DBAccess
+{
+    public class DB
+    {
+        string jsonFile = "appsetting.json";
+        private readonly string _connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\Taha\Term 4\Ap\Project\AP-Project-402-2\RestaurantDB\RestaurantDB.mdf;Integrated Security=True;Connect Timeout=30";
+        SqlConnection con;
+        public DB()
+        {
+            con = new SqlConnection(_connectionString);
+        }
+
+        public void Main(string[] args)
+        {
+
+            con.Open();
+            Console.WriteLine("Run properly");
+            con.Close();   
+        }
+
+        
+
+    }
+}
+  
