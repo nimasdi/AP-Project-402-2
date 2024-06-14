43af432ce240f3384d5d699deb48b0271d3ff02c
Taha Biklaryan <taha.biglarian@gmail.com>
2024-06-14 18:47:18 +0330

    Data Base Access Functions

---
+++ Restaurant/DBAccess/DataAccess.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.SqlClient; 
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlTypes;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Dapper;
+
+
+
+namespace DBAccess
+{
+    public class DataAccess
+    {
+        string jsonFile = "appsetting.json";
+        private readonly string _connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\Taha\Term 4\Ap\Project\AP-Project-402-2\RestaurantDB\RestaurantDB.mdf;Integrated Security=True;Connect Timeout=30";
+        
+        //SqlConnection con;
+
+        //Loading data from DB
+        public List<T> LoadData<T, U>(string sqlStatement, U parameters)
+        {
+            using(IDbConnection connection = new SqlConnection(_connectionString))
+            {
+                List<T> rows = connection.Query<T>(sqlStatement, parameters).ToList();
+                return rows;
+            }
+        }
+
+        //Inserting data to DB
+        public void SaveData<T>(string sqlStatement, T parameters)
+        {
+            using(IDbConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Execute(sqlStatement, parameters); 
+            }
+        } 
+
+        
+
+    }
+}
+  
