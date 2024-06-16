2e3a50dc1a95de59d27eadcdb8d0bf0946112b88
Nima Saeidi <ni.saeidi83@gmail.com>
2024-06-16 16:39:30 +0330

    created the base Admin class

---
+++ Restaurant/Project's classes/Admin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_s_classes
+{
+    internal class Admin
+    {
+        int AdminId;
+        string FirstName;
+        string LastName;
+        string MobileNumber;
+        string Email;
+        string UserName;
+        string Password;
+        string Address;
+        string Gender;
+
+        public Admin(int adminId, string firstName, string lastName, string mobileNumber, string email, string userName, string password, string address, string gender)
+        {
+            AdminId = adminId;
+            FirstName = firstName;
+            LastName = lastName;
+            MobileNumber = mobileNumber;
+            Email = email;
+            UserName = userName;
+            Password = password;
+            Address = address;
+            Gender = gender;
+        }
+
+    }
+}
