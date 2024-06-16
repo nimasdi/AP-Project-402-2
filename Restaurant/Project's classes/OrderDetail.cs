867158ac5f4f1e0cbb68c6b6f54681047ef14e8f
Nima Saeidi <ni.saeidi83@gmail.com>
2024-06-16 19:07:11 +0330

    created the base OrderDetail class

---
+++ Restaurant/Project's classes/OrderDetail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_s_classes
+{
+    internal class OrderDetail
+    {
+        int OredrDetailId;
+        int? OrderId;
+        int? MenuId;
+        int? Quantity;
+        decimal? Price;
+    }
+}
