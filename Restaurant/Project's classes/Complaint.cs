5f8a777ffc24f4261e7441aafd6a01dc5e0eabe7
Nima Saeidi <ni.saeidi83@gmail.com>
2024-06-16 16:46:36 +0330

    created the base Complaint class

---
+++ Restaurant/Project's classes/Complaint.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_s_classes
+{
+    internal class Complaint
+    {
+        int ComplaintId;
+        int? UserId;
+        int? RestaurantId;
+        string? Title;
+        string? Description;
+        string? Status;
+
+        public Complaint(int complaintId, int? userId, int? restaurantId, string? title, string? description, string? status)
+        {
+            ComplaintId = complaintId;
+            UserId = userId;
+            RestaurantId = restaurantId;
+            Title = title;
+            Description = description;
+            Status = status;
+        }
+    }
+}
