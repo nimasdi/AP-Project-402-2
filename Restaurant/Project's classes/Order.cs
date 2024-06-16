27e54d871d6e43ba93396fadf44b4936c2473208
Nima Saeidi <ni.saeidi83@gmail.com>
2024-06-16 19:12:20 +0330

    created the base Order class

---
+++ Restaurant/Project's classes/Order.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_s_classes
+{
+    internal class Order
+    {
+        int OrderId;
+        int? UserId;
+        int? RestuarantId;
+        DateTime? OrderDate;
+        decimal? TotalAmount;
+        string? PaymentMethod;
+        string? status;
+
+        public Order(int orderId, int? userId, int? restuarantId, DateTime? orderDate, decimal? totalAmount, string? paymentMethod, string? status)
+        {
+            OrderId = orderId;
+            UserId = userId;
+            RestuarantId = restuarantId;
+            OrderDate = orderDate;
+            TotalAmount = totalAmount;
+            PaymentMethod = paymentMethod;
+            this.status = status;
+        }
+    }
+}
