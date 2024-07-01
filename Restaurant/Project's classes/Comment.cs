47527c95caa92aab60eae85979927a03c1effcaa
Nima Saeidi <ni.saeidi83@gmail.com>
2024-07-01 19:38:41 +0330

    created comment class and edited menu class

---
+++ Restaurant/Project's classes/Comment.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_s_classes
+{
+    public class Comment
+    {
+        public int CommentID { get; set; }
+        public int MenuID { get; set; }
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string Content { get; set; }
+        public float Rating { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool Edited { get; set; }
+
+
+        public Comment(int commentID, int menuID, int userID, string userName, string content, float rating, DateTime createdAt, bool edited)
+        {
+            CommentID = commentID;
+            MenuID = menuID;
+            UserID = userID;
+            UserName = userName;
+            Content = content;
+            Rating = rating;
+            CreatedAt = createdAt;
+            Edited = edited;
+        }
+    }
+}
--- Restaurant/Project's classes/Menu.cs
+++ Restaurant/Project's classes/Menu.cs
@@ -6,17 +6,17 @@ using System.Threading.Tasks;
 
 namespace Project_s_classes
 {
-    internal class Menu
+    public class Menu
     {
-        int MenuID {  get; set; }
-        int? RestaurantId {  get; set; }
-        string? Category {  get; set; }
-        string? ItemName {  get; set; }
-        string? Ingredients {  get; set; }
-        decimal? Price {  get; set; }
-        string? ImageURL {  get; set; }
-        float? AverageRating {  get; set; }
-        int? QuantityAvailable {  get; set; }
+        public int MenuID {  get; set; }
+        public int? RestaurantId {  get; set; }
+        public string? Category {  get; set; }
+        public string? ItemName {  get; set; }
+        public string? Ingredients {  get; set; }
+        public decimal? Price {  get; set; }
+        public string? ImageURL {  get; set; }
+        public float? AverageRating {  get; set; }
+        public int? QuantityAvailable {  get; set; }
 
         public Menu(int? restaurantId, string? category, string? itemName, string? ingredients, decimal? price, string? imageURL, float? averageRating, int? quantityAvailable)
         {
